6cfd9b9470bd36ee54359b29aee45a10edc27940
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-02-14 21:30:40 +0100

    Add authentication pages and components: Login, Register, and layout

---
+++ src/components/auth/login-button.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+interface LoginButtonProps {
+  children: React.ReactNode;
+  mode: "modal" | "redirect";
+  asChild: boolean;
+}
+
+import React from "react";
+import { useRouter } from "next/navigation";
+
+const LoginButton = ({
+  children,
+  mode = "redirect",
+  asChild = false,
+}: LoginButtonProps) => {
+  const router = useRouter();
+  const onClick = () => {
+    router.push("/auth/login");
+  };
+
+  if (mode === "modal") {
+    return <span>TODO Implement modal!</span>;
+  }
+
+  return (
+    <span onClick={onClick} className="cursor-pointer">
+      {children}
+    </span>
+  );
+};
+
+export default LoginButton;
