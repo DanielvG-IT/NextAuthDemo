f489d8558c03cd79b7c72fc48eb7ddd5786e1495
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-02-15 01:09:08 +0100

    Enhance NextAuth integration with user role management and session handling

--- src/auth.ts
+++ src/auth.ts
@@ -1,18 +1,43 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 import { db } from "@/lib/db";
-// import { PrismaClient } from "@prisma/client";
+import { UserRole } from "@prisma/client";
+import { getUserById } from "@/data/user";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   callbacks: {
+    // async signIn({ user }) {
+    //   const existingUser = await getUserById(user.id);
+
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+    //   return true;
+    // },
     async session({ session, token }) {
       if (token.sub && session.user) {
         session.user.id = token.sub as string;
       }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
       return session;
     },
     async jwt({ token }) {
+      if (!token.sub) {
+        return token;
+      }
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) {
+        return token;
+      }
+
+      token.role = existingUser.role;
       return token;
     },
   },
