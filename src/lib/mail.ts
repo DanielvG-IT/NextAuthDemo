4c205acc80ad8ecf3778e4201d843aee1b3eef23
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-02-16 18:35:51 +0100

    FEATURE: Update email sender addresses to use environment variables for verification and password reset emails

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,13 +1,16 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const publicUrl = process.env.PUBLIC_URL;
+const publicUrl = process.env.NEXT_PUBLIC_APP_URL;
+const verifyEmailFromEmail = process.env.VERIFY_MAIL_FROM_ADDRESS;
+const passwordResetFromEmail = process.env.RESET_PASSWORD_MAIL_FROM_ADDRESS;
+const twoFactorTokenFromEmail = process.env.TWOFACTORTOKEN_MAIL_FROM_ADDRESS;
 
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmationLink = `${publicUrl}/auth/verify-email?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: `${verifyEmailFromEmail}`,
     to: email,
     subject: "Verify your email address",
     html: `<p>Click <a href="${confirmationLink}">here</a> to verify your email address.</p>`,
@@ -18,7 +21,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const ResetPasswordLink = `${publicUrl}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: `${passwordResetFromEmail}`,
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${ResetPasswordLink}">here</a> to reset your password.</p>`,
@@ -27,7 +30,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: `${twoFactorTokenFromEmail}`,
     to: email,
     subject: "Login 2FA Code",
     html: `<p>Your two-factor authentication token is: <strong>${token}</strong></p>`,
